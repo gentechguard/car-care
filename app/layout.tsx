967287a279edb28eb38d51d27d3fd0e6c060f10e
Sai Gowtham <puvvulasaigowtham@gmail.com>
2026-05-16 13:35:42 +0530

    update the code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { GlobalProvider } from "@/context/GlobalStore";
 import WhatsAppButton from "@/components/WhatsAppButton";
@@ -20,6 +21,8 @@ export const metadata: Metadata = {
   description: "Next-generation automotive protection solutions backed by industry expertise and advanced Aliphatic TPU technology.",
 };
 
+const GTM_ID = "GTM-NQLWDL64";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -27,11 +30,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth" suppressHydrationWarning style={{ background: '#070604' }}>
+      <head>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#070604] text-white`}
         suppressHydrationWarning
         style={{ background: '#070604', color: '#fff' }}
       >
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <GlobalProvider>
           <ScrollToTop />
           {children}
